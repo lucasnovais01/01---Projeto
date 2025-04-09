4330ba6950b3119cade26a193e544909ebede71e
lucasnovais01 <novais.oliveira@aluno.ifsp.edu.br>
2025-04-08 22:25:04 -0300

    mudanças

--- js/classes-atributos.js
+++ js/classes-atributos.js
@@ -4,34 +4,36 @@ console.log("Classes e atributos: ");
 //Retorna uma lista com as classes
 //do elemento . Permite adicionar, remover e verificar se cóntem algum valor especifico que vc quer
 
-console.log("Parte 1: ");
+console.log("Parte 1: classList e Name");
 
-const menu = document.querySelector('.menu');
+//const menu = document.querySelector('.menu');
 
 //console.log(menu); //costume pra ver se está pegando o javascript
 
-console.log(menu.classList); // []Traz uma array string
+//console.log(menu.classList); // []Traz uma array string
 
-console.log(menu.className) // string
+//console.log(menu.className) // string
 
 //Para adicionar um elemento ao menu por exemplo
 
-menu.classList.add('ativo'); // adiciona um "elemento" ao componente 'tag.class html'
+//menu.classList.add('ativo'); // adiciona um "elemento" ao componente 'tag.class html'
 
-menu.classList.add('ativo', 'mobile'); //mais de um, tem que colocar virgula
+//menu.classList.add('ativo', 'mobile'); //mais de um, tem que colocar virgula
 
-menu.classList.remove('mobile'); // agora só vai ter o ativo, pois ele removeu
+//menu.classList.remove('mobile'); // agora só vai ter o ativo, pois ele removeu
 
-menu.classList.toggle('ativo'); // se não existir - ele adiciona, se não existir ele adiciona
+//menu.classList.toggle('ativo'); // se não existir - ele adiciona, se não existir ele adiciona
 //toggle de ligado e desligado só lembrar
 
-menu.classList.contains('ativo'); 
+//menu.classList.contains('ativo'); 
 // 
-const contem = menu.classList.contains('ativo'); //true ou false
+//const contem = menu.classList.contains('ativo'); //true ou false
 
 //console.log(contem);
 
-menu.classList.replace('ativo', 'inativo');
+//menu.classList.replace('ativo', 'inativo');
+//***********
+console.log("Parte 2: Attribute - retorna um array")
 
 //atributes - retorna um array-like com os elementos
 /*// tem erro no de baixo
@@ -60,7 +62,7 @@ console.log(animais.attributes); //trás todos os atributos do elemento e també
 //img.removeAttribute('alt');
 
 // *************
-
+console.log("Parte 3: Read Only e Writeable: ");
 // Read Only e Writable:
 //propriedades que são somente de leitura
 //propriedades que permitem alteração
@@ -77,4 +79,59 @@ meusAnimais.className+='vermelho';
 
 meusAnimais.attribute = 'class="ativo"';
 
+console.log("Parte 4: Objeto propriedade: ");
 //objeto.propriedade
+
+//Desafio:
+//1 - Adicione a classe ativo a todos os item do menu
+
+//2 - Remova a classe Ativo e todos os itens do menu,
+//mantenha apenas no primeiro
+
+
+//3 - Verifique se as imagens possuem o attribute Alt
+
+//4 - Modifique o href do link externo do menu
+//bri.ifsp.edu.br => www.google.com.br
+
+console.log("1 - Adicione a classe ativo a todos os item do menu: ");
+
+const itensMenu = document.querySelectorAll('.menu a'); // assim vai selecionar os links, ".menu a"
+
+//console.log(itensMenu); //dica pra ver se está funcionando
+
+itensMenu.forEach((item)=>{
+  item.classList.add('ativo'); //a classe ativo dentro do href do li do menu
+});
+
+console.log("2 - Remova a classe Ativo e todos os itens do menu, mantenha apenas no primeiro");
+
+itensMenu.forEach((item)=>{
+  item.classList.remove('ativo');
+});
+//manter só o primeiro
+itensMenu[0].classList.add('ativo');
+
+console.log("3 - Verifique se as imagens possuem o attribute Alt");
+
+const pegarImagens = document.querySelectorAll('img');
+
+// console.log(pegarImagens); // ver se está funcionando
+
+pegarImagens.forEach((img) => {
+  const possuiAlt = img.hasAttribute('alt');
+  console.log(img, possuiAlt);
+});
+
+console.log("4 - Modifique o href do link externo do menu");
+
+const link = document.querySelectorAll('a[href^="http"]');
+//ajuda 
+const links = document.querySelectorAll('a[href^="http"]');
+
+// Itera sobre cada link e modifica o href
+links.forEach((link) => {
+  link.setAttribute('href', 'https://www.google.com.br');
+});
+
+console.log(links);
