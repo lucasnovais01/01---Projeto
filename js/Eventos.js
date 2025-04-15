2eff93223e6bee4ca79b66f03ba35740dd5a4c26
lucasnovais01 <novais.oliveira@aluno.ifsp.edu.br>
2025-04-15 20:38:44 -0300

    addEventListener

--- js/Eventos.js
+++ js/Eventos.js
@@ -1,7 +1,77 @@
-console.log("Classes e atributos: ");
+console.log("05 - Eventos: ");
 
-//  classlist
-//Retorna uma lista com as classes
-//do elemento . Permite adicionar, remover e verificar se cóntem algum valor especifico que vc quer
+//  Eventos
 
-console.log("Parte 1: classList e Name");
+console.log("Parte 1: ");
+
+img = document.querySelector('img');
+
+function callback(e){ //recomendado em usar o callback
+  console.log('clicou');
+}
+
+console.log(img);
+
+img.addEventListener('click', callback); // Tem sempre dois parâmetros, o primeiro é o que espera que faz, e o segundo é a função
+
+img.addEventListener('click', function(){
+  console.log('clicou');
+});
+
+// primeiro parâmetro é ação = esperada
+
+// segundo parâmetro é a função a ser executada. O call back
+
+const animaisLista = document.querySelector('.animal-item');
+
+console.log(animaisLista);
+
+
+function executaCallBack(e){
+  
+  const currentTarget = e.currentTarget;
+  const target = e.target;
+  const type = e.type;
+  const path = e.path; // pra funcionar precisa do composedPath(), porque a propriedade path não é uma propriedade padrão do objeto Event em todos os navegadores. Ela era uma propriedade não padrão suportada apenas pelo Chrome, mas foi removida em versões mais recentes.
+
+  console.log(currentTarget);
+  console.log(target);
+  console.log(type);
+  console.log(path);
+  /*
+  console.log( 
+    currentTarget,
+    target,
+    type,
+    path
+    );
+*/
+}
+
+animaisLista.addEventListener('click', executaCallBack);
+
+const link = document.querySelector('a[href^="http"]');
+
+function clickNoLink(e){
+  e.preventDefault(); // evita o evento padrão, carregamento das páginas
+  
+  console.log(this); // this - palavra reservada do javascript, serve para fazer referência a próprio objeto.
+  
+  //console.log(e.currentTarget.href);
+  console.log(this.getAttribute('href'));
+}
+
+//link.addEventListener('click');
+
+function callKey(e){
+  if (e.key === 'a'){
+    document.body.classList.toggle('azul');
+  }
+  else {
+    if (e.key='v') {
+      document.body.classList.toggle('verde');
+    }
+  }
+}
+
+window.addEventListener('keydown', callKey);
