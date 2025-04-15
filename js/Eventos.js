75d654347a41609350b17719f9b939f7399ea94f
lucasnovais01 <novais.oliveira@aluno.ifsp.edu.br>
2025-04-15 19:54:43 -0300

    05 - Eventos

---
+++ js/Eventos.js
@@ -0,0 +1,7 @@
+console.log("Classes e atributos: ");
+
+//  classlist
+//Retorna uma lista com as classes
+//do elemento . Permite adicionar, remover e verificar se cóntem algum valor especifico que vc quer
+
+console.log("Parte 1: classList e Name");
