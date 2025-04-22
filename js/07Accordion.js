8d6c69a4f14e74d963921c61ee90d586da475eb3
lucasnovais01 <novais.oliveira@aluno.ifsp.edu.br>
2025-04-22 20:14:58 -0300

    Scroll-suave

---
+++ js/07Accordion.js
@@ -0,0 +1,32 @@
+
+console.log("Accordion");
+
+
+// initTABNav();
+
+initAccordion();
+
+function initAccordion() {
+
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+
+  const classeAtiva = 'ativo'; // só pra organizar
+
+  accordionList[0].classList.add('ativo');      // Inclui no primeiro elemento
+  accordionList[0].nextElementSibling.classList.add('ativo');
+  
+  function activeAccordion(){
+
+    this.classList.toggle('ativo');   // da pra colocar entre os parenteses (classeAtiva)          // Inclui nos demais elementos (ou remove por causa do toggle)
+    this.nextElementSibling.classList.toggle('ativo');    // da pra colocar entre os parenteses (classeAtiva)
+
+  }
+
+  initAccordion.forEach(( item) =>{
+    
+      item.addEventListener('click', activeAccordion);
+    });
+}
+
+
+/* NÃO FUNCIONOU NO FINAL DA AULA - O PROFESSOR TESTOU NO FAQ */
---
+++ js/scrollSuave.js
@@ -0,0 +1,3 @@
+console.log("08 - Scroll suave");
+
+
