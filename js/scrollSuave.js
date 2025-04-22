eaf3811a241a1e1b862c0b21ac4ea276edca4b99
lucasnovais01 <novais.oliveira@aluno.ifsp.edu.br>
2025-04-22 20:35:53 -0300

    dd

--- js/scrollSuave.js
+++ js/scrollSuave.js
@@ -13,14 +13,17 @@ function initAccordion(){
     e.preventDefault(); //vai evitar que a página recarrega, isto vai acontecer ao clicar por causa do callback, causando uma transição mais suave
     
     const href = e.currentTarget.getAttribute('href');
-  
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior :'smooth',   //transição suave
+      block:'start',        //no início
+    })
   }
-
-
 
   linkInternos.forEach((item)=> { //arrow function
     
-    item.addEventListener('click', scrolltoSection);
+    item.addEventListener('click',scrolltoSection);
   });
 
   console.log (linkInternos);
