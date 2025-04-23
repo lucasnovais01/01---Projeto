c37da0117d87ac6492027d14807bba870b938517
lucasnovais01 <novais.oliveira@aluno.ifsp.edu.br>
2025-04-22 21:45:39 -0300

    consertado

--- js/scrollSuave.js
+++ js/scrollSuave.js
@@ -1,11 +1,33 @@
 console.log("08 - Scroll suave");
 
-
-/* --- Acima os Comandos do 07Accordion --- */
-
+/*           */
 initAccordion();
 
-function initAccordion(){
+function initAccordion() {
+
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+
+  const classeAtiva = 'ativo'; // só pra organizar
+
+  accordionList[0].classList.add(classeAtiva);     // Inclui no primeiro elemento
+  accordionList[0].nextElementSibling.classList.add(classeAtiva);
+  
+  function activeAccordion(){
+
+    this.classList.toggle(classeAtiva);   // da pra colocar entre os parenteses (classeAtiva)          // Inclui nos demais elementos (ou remove por causa do toggle)
+    this.nextElementSibling.classList.toggle(classeAtiva);    // da pra colocar entre os parenteses (classeAtiva)
+
+  }
+
+  accordionList.forEach((item) => {
+    item.addEventListener('click', activeAccordion);
+  });
+}
+/* --- Acima os Comandos do 07Accordion --- */
+
+//initAccordion();
+
+function initScrollSuave(){
 
   const linkInternos = document.querySelectorAll('.js-menu a[href^="#"]'); //todos que comecem com o # dentro do menu
 
@@ -18,10 +40,10 @@ function initAccordion(){
     section.scrollIntoView({
       behavior :'smooth',   //transição suave
       block:'start',        //no início
-    })
+    });
   }
 
-  linkInternos.forEach((item)=> { //arrow function
+  linkInternos.forEach((item) => { //arrow function
     
     item.addEventListener('click',scrolltoSection);
   });
